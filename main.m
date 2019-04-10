65bf6cf595ad1311d04d1f0f90033e5ca44952d8
Roger <sunrimii@gmail.com>
2019-04-10 18:12:52 +0800

    test

--- main.m
+++ main.m
@@ -2,7 +2,14 @@ monster = Monster();
 
 while 1 
     for A = monster.body
-       monster.update(A)
-       pause(0.01)
+       monster.update(monster.pos_of_knee1, A);
+       monster.update(monster.pos_of_knee2, A);
+       % [cos(pi/3) -sin(pi/3); sin(pi/3) cos(pi/3)]
+       monster.update(monster.pos_of_knee3, [cos()] * A);
+       monster.update(monster.pos_of_knee4, [] * A);
+       
+       hold off;
+       axis([-15 15 -15 15]);
+       pause(0.01);
     end
 end
