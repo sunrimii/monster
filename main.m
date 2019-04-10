f7259a222c716d1c1c053f75f5815e13cb06178b
卓家禾 <sunrimii@gmail.com>
2019-04-10 20:09:04 +0800

    Update main.m

--- main.m
+++ main.m
@@ -5,11 +5,13 @@ while 1
        monster.update(monster.pos_of_knee1, A);
        monster.update(monster.pos_of_knee2, A);
        % [cos(pi/3) -sin(pi/3); sin(pi/3) cos(pi/3)]
-       monster.update(monster.pos_of_knee3, [cos()] * A);
-       monster.update(monster.pos_of_knee4, [] * A);
+%        A = [0.5 -0.866; 0.866 0.5] * A;
+A = [0 -1; 1 0] * A;
+       monster.update(monster.pos_of_knee3, A);
+       monster.update(monster.pos_of_knee4, A);
        
        hold off;
        axis([-15 15 -15 15]);
-       pause(0.01);
+       pause(0.1);
     end
 end
