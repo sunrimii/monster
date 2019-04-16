612789d2661cdbfe3f8f91d88ec1b9120b974f2b
Roger <sunrimii@gmail.com>
2019-04-16 23:19:39 +0800

    Update

--- main.m
+++ main.m
@@ -1,8 +1,11 @@
 global monster
+
 monster = Monster();
 
+% 下方滑桿
 slider = uicontrol('style', 'slider', 'position', [20 10 500 20], 'min', 0, 'max', 2*pi);
 slider.Callback = @plot_monster;
+
 
 function plot_monster(slider, ~)
    global monster
@@ -18,9 +21,11 @@ function plot_monster(slider, ~)
       moving_pnt = [0 -1; 1 0] * moving_pnt; % 旋轉90度
       monster.plot_leg(moving_pnt, monster.pos_of_knee3, monster.color_of_knee3);
       monster.plot_leg(moving_pnt, monster.pos_of_knee4, monster.color_of_knee4);
-         
+
       hold off;
-      axis([-15 15 -15 15]);
+      axis([-20 20 -20 20]);
+
       pause(0.001);
    % end
 end
+
